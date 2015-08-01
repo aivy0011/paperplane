5b8d7ca4953a416af7acc8be2918c5bf47bc8170
aivy <aivy0011@gmail.com>
2015-08-01 09:46:10 -0400

    redesign the random color genrator and I updated the links

--- Javascript/rcolor.js
+++ Javascript/rcolor.js
@@ -1,3 +1,11 @@
+function getContrastYIQ(hexcolor){
+	var r = parseInt(hexcolor.substr(0,2),16);
+	var g = parseInt(hexcolor.substr(2,2),16);
+	var b = parseInt(hexcolor.substr(4,2),16);
+	var yiq = ((r*299)+(g*587)+(b*114))/1000;
+	return (yiq >= 128) ? 'black' : 'white';
+}
+
 function randomcolor() {
 	var rString = ['A','B','C','D','E','F','0','1','2','3','4','5','6','7','8','9'];
 	var Hexcode = '#';
@@ -6,10 +14,10 @@ function randomcolor() {
 		var Hexcode = Hexcode + rString[randomn];
 		document.body.style.background = Hexcode;
 		document.getElementById('Hexcode').innerHTML = Hexcode;
-		document.getElementById('Hexcode').style.color = Hexcode;
-		document.getElementById('Heading').style.color = Hexcode;
 		console.log(Hexcode);
 	}
+	document.getElementById('Hexcode').style.color = getContrastYIQ(Hexcode);
+document.getElementById('Heading').style.color = getContrastYIQ(Hexcode);
 	start();
 }
 function start() {
