8c62e7b6178d36addd607b00118ca48f11fe215e
aivy <aivy0011@gmail.com>
2015-08-05 20:06:56 -0400

    Updated Links

--- Javascript/Pig_Latin.js
+++ Javascript/Pig_Latin.js
@@ -1,5 +1,25 @@
-function translate() {
+function contains(a, obj) {
+    var i = a.length;
+    while (i--) {
+       if (a[i] === obj) {
+           return true;
+       }
+    }
+    return false;
+}
+var vowels = ["a","A","e","E","i","I","o","O","u","U"]
+var result = ""
+function t() {
 	var string = document.getElementById('string').value;
-	var split = split.split(" ")
-	document.getElementById('result').innerHTML = split;
+	var string = string.split(" ");
+	for(i = 0; i < string.length; i++) {
+		var word = string[i]
+		var fletter = word.substring(0);
+		if (contains(vowels, fletter)){
+			result = result + word + "way ";
+		} else {
+			result = result + word.substring(1, (word.length - 1)) + fletter + "ay ";
+		}
+	}
+	document.getElementById("result").innerHTML = result;
 }
