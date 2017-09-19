5669daa44f91551eb5c5e53835848d9f045c2316
Allen Ivy <aivy0011@gmail.com>
2017-09-19 16:19:01 -0400

    Finish converting Loan Calculator to Javascript

---
+++ Javascript/lc.js
@@ -0,0 +1,38 @@
+function round(value, precision) {
+    var multiplier = Math.pow(10, precision || 0);
+    return Math.round(value * multiplier) / multiplier;
+}
+function calculatePayment(loanAmt, annualIntRate, numMons){
+    var Months_Per_Year = 12
+    annualIntRate = annualIntRate / 100 / Months_Per_Year;
+    var numerator = annualIntRate * Math.pow(1 + annualIntRate, numMons);
+    var denominator = Math.pow(1 + annualIntRate, numMons) - 1;
+    return round((loanAmt * (numerator/denominator)), 2);
+}
+function display(){
+    var Maximum_Interest_Rate = parseInt($("#MIR").val());
+    var Number_Of_Months = parseInt($("#LOL").val());    
+    var Low_Loan_Amount = parseInt($("#LL").val());
+    var High_Loan_Amount = parseInt($("#HL").val());
+    var Loan_Increment = parseInt($("#LI").val());
+
+    var table = "<h1>Loan Table</h1><table><th></th>";
+    for(var loan = Low_Loan_Amount; loan <= High_Loan_Amount; loan += Loan_Increment){
+        table += "<th class='loan'>$"+loan+"</th>";
+    }
+    for(var interest = 1; interest <= Maximum_Interest_Rate; interest++){
+        table += "<tr><th class='interest'>"+interest+"%</th>";
+        for(var loan = Low_Loan_Amount; loan <= High_Loan_Amount; loan += Loan_Increment){
+            monthly = calculatePayment(loan, interest, Number_Of_Months)
+            table += "<td class='payments'>$"+monthly+"</td>";
+        }
+        table += "</tr>";
+    }
+    table += "</table><br><button class='btn btn' style='background-color: #2ecc71; color: white' data-toggle='modal' data-target='#myModal'>Change numbers</button>"
+    $("#content").html(table);
+}
+
+$(document).ready(function(){
+    display();
+    
+})
