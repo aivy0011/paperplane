0819b1cfbabf4ddc0bd1142f1f47efd9c17b6650
aivy0011 <aivy0011@gmail.com>
2020-07-29 21:03:19 -0400

    Updating website

--- practice.php
+++
@@ -1,5 +0,0 @@
-<?php
-$conn = new mysqli('localhost', 'root', '1234','Ajax');
-$data = mysqli_fetch_all(mysqli_query($conn, "SELECT * FROM Practice"));
-echo json_encode($data);
-?>
--- test/index.php
+++
@@ -1,9 +0,0 @@
-<?php include("class_lib.php"); ?>
-<?php
-    $Allen = new person("Allen Ivy");
-    $Christopher = new person("Christopher Ivy");
-    
-    echo "Allen full name is ". $Allen->get_name();
-    $james = new employee("Johnny Fingers");
-		echo "---> " . $james->get_name();
-?>
